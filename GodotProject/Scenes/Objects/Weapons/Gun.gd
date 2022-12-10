cf0189af0f9fa15563704850b45314f84dae38e1
plexsoup <plexsoup@gmail.com>
2022-12-09 17:25:22 -0800

    basic gun and bullet

--- GodotProject/Scenes/Objects/Weapons/Gun.gd
+++ GodotProject/Scenes/Objects/Weapons/Gun.gd
@@ -1,17 +1,30 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var current_ammo
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if current_ammo == null:
+		current_ammo = $Ammo/Bullet
+
+func _unhandled_input(event):
+	if event.is_action_pressed("shoot"):
+		shoot(current_ammo)
+	else:
+		printerr("config error in Gun.gd, _unhandled_input(). Ammo has no shoot method.")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func shoot(ammo):
+	
+	var new_projectile = ammo.duplicate()
+	
+	# a signal to the map would be nicer, but this works for now
+	get_parent().add_child(new_projectile)
+	new_projectile.init($MuzzlePosition.global_position, $MuzzlePosition.global_rotation)
+	
+
+func _process(_delta):
 	# temporary aiming mechanic
 	look_at(get_global_mouse_position())
--- GodotProject/Scenes/Player/PlayerAvatar.gd
+++ GodotProject/Scenes/Player/PlayerAvatar.gd
@@ -9,7 +9,7 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	velocity = Vector2.ZERO
 	
 	if Input.is_action_pressed("ui_left"):
