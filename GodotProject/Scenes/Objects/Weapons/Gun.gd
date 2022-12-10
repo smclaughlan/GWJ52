ffe44102442369d3d5f5fff9299228a57a124e94
plexsoup <plexsoup@gmail.com>
2022-12-09 21:34:01 -0800

    modified gun a bit

--- GodotProject/Scenes/Objects/Weapons/Gun.gd
+++ GodotProject/Scenes/Objects/Weapons/Gun.gd
@@ -3,26 +3,32 @@ extends Node2D
 
 var current_ammo
 
+enum States { INITIALIZING, READY, FIRING, COCKING, RELOADING, DEAD }
+var State = States.INITIALIZING
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if current_ammo == null:
 		current_ammo = $Ammo/Bullet
 
+	State = States.READY
+	
 func _unhandled_input(event):
 	if event.is_action_pressed("shoot"):
 		shoot(current_ammo)
 
 
 func shoot(ammo):
-	
-	var new_projectile = ammo.duplicate()
-	
-	# a signal to the map would be nicer, but this works for now
-	Global.stage_manager.current_map.add_child(new_projectile)
-	if new_projectile.has_method("init"):
-		new_projectile.init($MuzzlePosition.global_position, $MuzzlePosition.global_rotation)
-	
+	if State == States.READY:
+		$BangNoise.play()
+		var new_projectile = ammo.duplicate()
+		
+		# a signal to the map would be nicer, but this works for now
+		Global.stage_manager.current_map.add_child(new_projectile)
+		if new_projectile.has_method("init"):
+			new_projectile.init($MuzzlePosition.global_position, $MuzzlePosition.global_rotation)
+		State = States.COCKING
+		$CockTimer.start()
 
 func _process(_delta):
 	var mousePos = get_global_mouse_position()
@@ -34,3 +40,12 @@ func _process(_delta):
 	else:
 		scale.x = abs(scale.x)
 		look_at(get_global_mouse_position())
+
+
+func _on_CockTimer_timeout():
+	State = States.READY
+	
+	# continuous fire by holding mouse button down
+	if Input.is_action_pressed("shoot"):
+		shoot(current_ammo)
+	
