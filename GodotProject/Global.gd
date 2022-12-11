baba4c3fd993b15152cf03b5ccc17a8673b40686
rcsira <sirarenz@gmail.com>
2022-12-11 10:04:40 +0800

    Added `currency_tracker` in `Global.gd`

--- GodotProject/Global.gd
+++ GodotProject/Global.gd
@@ -6,5 +6,4 @@ var player : KinematicBody2D # player avatar, many things rely on this
 var village_location : Vector2 # so creeps know where to go
 var current_map : Node2D # for spawning bullets and creeps
 var pickable_object_spawner: Node2D # Spawner node that handles spawning of pickables
-
-
+var currency_tracker: Node # Node that tracks resources or currencies
