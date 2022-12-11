345e0da8919a2a0e28f34b39fae4637e339da10a
rcsira <sirarenz@gmail.com>
2022-12-11 09:16:41 +0800

    Pickables move towards the Player when Radius is entered

--- GodotProject/Scenes/Objects/Pickups/PickableObject.gd
+++ GodotProject/Scenes/Objects/Pickups/PickableObject.gd
@@ -1,6 +1,28 @@
 # Template for objects that are pickable
+#
+# Can be extended for different objects that provide different
+# functions.
+#
+# Once the player is in range, the item will be freed instantly
+
 extends Area2D
 
-func _ready() -> void:
-	connect("body_entered", Global.player, "on_PickableObjects_body_entered")
+const PICKUP_SPEED = 300
 
+var on_pickup_radius: bool = false setget set_on_pickup
+
+func _on_PickableObject_body_entered(body: Node) -> void:
+	if body.collision_layer == 1:
+		queue_free()
+
+
+func _physics_process(delta: float) -> void:
+	if !on_pickup_radius:
+		return
+	var velocity: Vector2 = position.direction_to(Global.player.position)
+	position += velocity * PICKUP_SPEED * delta
+	
+
+
+func set_on_pickup(value: bool) -> void:
+	on_pickup_radius = value
--- GodotProject/Scenes/Player/PickupRadius.gd
+++ GodotProject/Scenes/Player/PickupRadius.gd
@@ -1,2 +1,15 @@
 extends Area2D
 
+
+
+func _on_PickupRadius_area_entered(area: Area2D) -> void:
+	set_pickable_on_pickup(area, true)
+
+
+func _on_PickupRadius_area_exited(area: Area2D) -> void:
+	set_pickable_on_pickup(area, false)
+
+
+func set_pickable_on_pickup(area: Area2D, value: bool) -> void:
+	if area.has_method("set_on_pickup"):
+		area.on_pickup_radius = value
