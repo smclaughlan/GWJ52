34010459e9efb9cfbae523cf7b80dbfacf9e7fb1
plexsoup <plexsoup@gmail.com>
2022-12-11 15:23:05 -0800

    bugfix in get_closest_object
    is_instance_valid wasn't sufficient to weed out invalid objects, so I added is_inside_tree

--- GodotProject/Global.gd
+++ GodotProject/Global.gd
@@ -23,7 +23,7 @@ func get_closest_object(group : Array, referenceObj ):
 	var refPos = referenceObj.global_position
 	var closest_object = null
 	for obj in group:
-		if is_instance_valid(obj) and obj != referenceObj:
+		if is_instance_valid(obj) and obj != referenceObj and obj.is_inside_tree():
 			var objPos = obj.global_position
 			if closest_object == null or objPos.distance_squared_to(refPos) < closest_object.global_position.distance_squared_to(refPos):
 				closest_object = obj
