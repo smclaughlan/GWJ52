51d75e34620b270dc3c0a06a08d3616ba5e08609
smclaughlan <sean.s.mclaughlan@gmail.com>
2022-12-18 03:16:59 -0800

    Remove zoom toggle, set to 1.6

--- GodotProject/Scenes/Player/Camera2D.gd
+++ GodotProject/Scenes/Player/Camera2D.gd
@@ -1,18 +1,18 @@
 extends Camera2D
 
-var zoomed_in = true
+#var zoomed_in = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-func _unhandled_input(_event):
-	if Input.is_action_just_pressed("zoom_toggle"):
-		zoomed_in = !zoomed_in
-		if zoomed_in:
-			set_zoom(Vector2.ONE*1.6)
-		else:
-			set_zoom(Vector2.ONE*2.2)
+#func _unhandled_input(_event):
+#	if Input.is_action_just_pressed("zoom_toggle"):
+#		zoomed_in = !zoomed_in
+#		if zoomed_in:
+#			set_zoom(Vector2.ONE*1.6)
+#		else:
+#			set_zoom(Vector2.ONE*2.0)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
